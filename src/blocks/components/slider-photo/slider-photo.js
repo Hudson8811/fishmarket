20c4953073919d4130e695c3405128746ef325d6
Alexander Wh <111kanzas111@mail.ru>
2024-02-22 16:03:00 +0300

    product page finished, logistics page created

--- src/blocks/components/slider-photo/slider-photo.js
+++ src/blocks/components/slider-photo/slider-photo.js
@@ -7,13 +7,19 @@ function sliderPhotoInit() {
     let sliderPhotoThumbs = sliderPhotoWrapper.querySelector('[data-js="sliderPhotoThumbs"]')
 
     let sliderPhotoThumbsEx = new Swiper(sliderPhotoThumbs, {
-        spaceBetween: 18,
+        spaceBetween: 11,
         slidesPerView: 7,
         loop: true,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+        breakpoints: {
+            993: {
+                spaceBetween: 18,
+                slidesPerView: 7,
+            }
+        }
     });
 
     let sliderPhotoSliderEx = new Swiper(sliderPhotoSlider, {
