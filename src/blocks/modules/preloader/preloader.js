edf137ffe11d8b295db6cd66c24d847517c14bb7
Alexander Wh <111kanzas111@mail.ru>
2024-02-27 17:10:52 +0300

    preloader fix, logistic  map fix

--- src/blocks/modules/preloader/preloader.js
+++ src/blocks/modules/preloader/preloader.js
@@ -2,8 +2,8 @@ let preloaderSection = document.querySelector(".preloader__section")
 if(preloaderSection !== null ) {
 	setTimeout(() => {
 		preloaderSection.classList.add("no-active")
-	}, 2100)
+	}, 3100)
 	setTimeout(() => {
 		preloaderSection.style.display = "none"
-	}, 3100)
+	}, 4500)
 }
